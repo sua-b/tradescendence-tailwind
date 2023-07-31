acc15df5eb6e8763aeb36df06986cdc85da70706
sua-b <sua518721@hotmail.com>
2023-07-31 19:20:42 +0700

    move sidebar

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -4,7 +4,7 @@ import Sidebar from './Sidebar';
 
 export default function AppLayout() {
   return (
-    <div className='grid grid-cols-1 grid-rows-[auto_1fr_auto] md:grid-cols-[1fr_12rem] md:grid-rows-[auto_1fr] h-[100dvh] w-[100vw] overflow-y-scroll'>
+    <div className='grid grid-cols-1 grid-rows-[auto_1fr_auto] md:grid-cols-[12rem_1fr] md:grid-rows-[auto_1fr] h-[100dvh] w-[100vw] overflow-y-scroll'>
       <Header />
       <main className='w-full p-8'>
         <Outlet />
--- src/ui/Sidebar v1.jsx
+++ src/ui/Sidebar v1.jsx
@@ -2,7 +2,7 @@ import Quotes from './Quotes';
 
 export default function Sidebar() {
   return (
-    <aside className='bg-gray-200 row-start-3 md:row-start-2 md:col-start-1 p-4'>
+    <aside className='bg-gray-200 row-start-3 md:row-start-2 md:col-start-2 p-4'>
       <Quotes />
     </aside>
   );
