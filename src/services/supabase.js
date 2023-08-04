b517b7587c76e30836853dde2416eb9ead8ef4ff
sua-b <sua518721@hotmail.com>
2023-08-04 21:05:04 +0700

    fix

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-export const supabaseUrl = 'https://sqciznpufcbafxhpysor.supabase.co';
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
